1506a62e0a6809a62c1793bf6e112067548dca7a
Riyaz Mansoor <riyaz.mansoor@gmail.com>
2023-10-29 15:42:22 +0500

    .

--- 308.js
+++ 308.js
@@ -58,6 +58,141 @@ for n in 2^n
 2^(-n) 5^(+n)
 7^+1  11^+1  19^-1
      */
+    function primeIndex(prime) {
+        switch (prime) {
+            case 2: return 0;
+            case 3: return 1;
+            case 5: return 2;
+            case 7: return 3;
+            case 11: return 4;
+            case 13: return 5;
+            case 17: return 6;
+            case 19: return 7;
+            case 23: return 8;
+            case 29: return 9;
+            default: throw "errorrr";
+        }
+    }
+    const NUM = new Array(10).fill(0);
+    function stepCount(fracIndex) {
+        let cnt = 0;
+        while (cnt < 1e3) {
+            console.log(`cnt=${cnt} frac=${fracIndex} ${NUM}`);
+            switch (fracIndex) {
+                case 0:
+                    const pow7 = NUM[primeIndex(7)];
+                    const pow13 = NUM[primeIndex(13)];
+                    if (pow7 > 0 && pow13 > 0) {
+                        if (pow7 <= pow13) {
+                            // this counts the steps for the repeated 0-1-0-1 cycles
+                            const steps = pow7;
+                            NUM[primeIndex(2)] += steps;
+                            NUM[primeIndex(3)] += steps;
+                            NUM[primeIndex(5)] -= steps;
+                            NUM[primeIndex(7)] -= steps;
+                            // frac 9
+                            NUM[primeIndex(11)] -= 1;
+                            NUM[primeIndex(13)] += 1;
+                            fracIndex = 4;
+                            cnt += steps + 1;
+                        }
+                        else {
+                            // this counts the steps for the repeated 0-1-0-1 cycles (plus on 0 execution)
+                            const steps = pow13;
+                            NUM[primeIndex(2)] += steps;
+                            NUM[primeIndex(3)] += steps;
+                            NUM[primeIndex(5)] -= steps;
+                            NUM[primeIndex(7)] -= steps;
+                            fracIndex = 0;
+                            cnt += steps;
+                        }
+                    }
+                    else {
+                        NUM[primeIndex(7)] -= 1;
+                        NUM[primeIndex(13)] -= 1;
+                        NUM[primeIndex(17)] += 1;
+                        if (NUM[primeIndex(3)] > 0 && NUM[primeIndex(17)] > 0) {
+                            fracIndex = 2;
+                        }
+                        else if (NUM[primeIndex(17)] > 0) {
+                            fracIndex = 8;
+                        }
+                        else {
+                            throw `case 0 :: ${NUM}`;
+                        }
+                        cnt += 1;
+                    }
+                    break;
+                case 2:
+                    {
+                        // frac 2
+                        NUM[primeIndex(19)] += 1;
+                        NUM[primeIndex(13)] -= 1;
+                        NUM[primeIndex(17)] -= 1;
+                        // cycle of frac 3-6-3-6
+                        const steps = NUM[primeIndex(2)];
+                        NUM[primeIndex(5)] += steps;
+                        NUM[primeIndex(2)] -= steps;
+                        // frac 7
+                        NUM[primeIndex(7)] += 1;
+                        NUM[primeIndex(11)] += 1;
+                        NUM[primeIndex(19)] -= 1;
+                        if (NUM[primeIndex(3)] > 0 && NUM[primeIndex(11)] > 0) {
+                            fracIndex = 4;
+                        }
+                        else if (NUM[primeIndex(11)] > 0) {
+                            NUM[primeIndex(11)] -= 1;
+                            NUM[primeIndex(13)] += 1;
+                            fracIndex = 0;
+                            cnt += 1;
+                        }
+                        cnt += steps + 1;
+                    }
+                    break;
+                case 4:
+                    // this counts the steps for the repeated 4-5-4-5 cycles
+                    const steps = NUM[primeIndex(3)];
+                    NUM[primeIndex(3)] = 0;
+                    NUM[primeIndex(7)] += steps;
+                    cnt += 2 * steps; // 2 steps for each cycle
+                    // goes to frac 10;
+                    NUM[primeIndex(11)] -= 1;
+                    NUM[primeIndex(13)] += 1;
+                    cnt += 1;
+                    fracIndex = 0;
+                    break;
+                case 8:
+                    NUM[primeIndex(17)] -= 1;
+                    fracIndex = 11;
+                    cnt++;
+                    if (NUM.slice(1).every(v => v === 0)) {
+                        console.log(`cnt=${cnt} power2=${NUM[0]}`);
+                    }
+                    break;
+                case 11:
+                    // frac 11
+                    const steps11 = NUM[2];
+                    NUM[3] = steps11;
+                    NUM[5] = steps11;
+                    cnt += steps11;
+                    // frac 12 (conditional)
+                    const steps12 = NUM[7];
+                    if (steps12 > 0) {
+                        NUM[7] = 0;
+                        cnt += steps12;
+                    }
+                    // frac 13
+                    NUM[5] += 1;
+                    NUM[11] += 1;
+                    cnt += 1;
+                    fracIndex = 4;
+                    break;
+                default:
+                    throw `error ${cnt} ${NUM}`;
+            }
+        }
+    }
+    E308.stepCount = stepCount;
     const f2PN = new common_1.PowerNumber().mul(3, -1).mul(17, -1).mul(2, -1).mul(23, 1).mul(7, 1).mul(11, 1).mul(19, -1);
     function wheel(pownum, frac) {
         const startTime = new Date().getMilliseconds();
@@ -148,7 +283,9 @@ for n in 2^n
         for (let i = 0; i < numers.length; i++) {
             console.log(`(${facNumers[i]})/(${facDenoms[i]})`);
         }
-        wheel(new common_1.PowerNumber(2, 1), 11);
+        // wheel(new PowerNumber(2, 1), 11);
+        NUM[0] = 1;
+        stepCount(11);
     }
     E308.run = run;
 })(E308 || (E308 = {}));
--- 308.ts
+++ 308.ts
@@ -7,11 +7,11 @@ namespace E308 {
 
     const primes: PrimeNumbers = new PrimeNumbers(1000);
 
-    const numers: Integer[] = [ 17, 78, 19, 23, 29, 77, 95, 77,  1, 11, 13, 15, 1, 55 ] ;
-    const denoms: Integer[]  = [ 91, 85, 51, 38, 33, 29, 23, 19, 17, 13, 11,  2, 7,  1 ] ;
+    const numers: Integer[] = [17, 78, 19, 23, 29, 77, 95, 77, 1, 11, 13, 15, 1, 55];
+    const denoms: Integer[] = [91, 85, 51, 38, 33, 29, 23, 19, 17, 13, 11, 2, 7, 1];
 
-    const facNumers: Prime[][] = numers.map( n => primes.factorise(n) );
-    const facDenoms: Prime[][] = denoms.map( n => primes.factorise(n) );
+    const facNumers: Prime[][] = numers.map(n => primes.factorise(n));
+    const facDenoms: Prime[][] = denoms.map(n => primes.factorise(n));
 
 
     export function brute(): void {
@@ -19,7 +19,7 @@ namespace E308 {
         for (let i = 0; i < numers.length; i++) {
             paths.set(i, new Set());
         }
-        const pn: PowerNumber = new PowerNumber(2, 1); 
+        const pn: PowerNumber = new PowerNumber(2, 1);
         let lastInd: Integer = -1;
         let cnt: Integer = 0;
         while (cnt < 1e8) {
@@ -64,6 +64,138 @@ for n in 2^n
 7^+1  11^+1  19^-1
      */
 
+    function primeIndex(prime: Prime): Integer {
+        switch (prime) {
+            case 2: return 0;
+            case 3: return 1;
+            case 5: return 2;
+            case 7: return 3;
+            case 11: return 4;
+            case 13: return 5;
+            case 17: return 6;
+            case 19: return 7;
+            case 23: return 8;
+            case 29: return 9;
+            default: throw "errorrr";
+        }
+    }
+
+    const NUM: Integer[] = new Array(10).fill(0);
+
+    export function stepCount(fracIndex: Integer): void {
+        let cnt: Integer = 0;
+        while (cnt < 1e3) {
+            console.log(`cnt=${cnt} frac=${fracIndex} ${NUM}`);
+            switch (fracIndex) {
+                case 0:
+                    const pow7: Integer = NUM[primeIndex(7)];
+                    const pow13: Integer = NUM[primeIndex(13)];
+                    if (pow7 > 0 && pow13 > 0) {
+                        if (pow7 <= pow13) {
+                            // this counts the steps for the repeated 0-1-0-1 cycles
+                            const steps: Integer = pow7;
+                            NUM[primeIndex(2)] += steps;
+                            NUM[primeIndex(3)] += steps;
+                            NUM[primeIndex(5)] -= steps;
+                            NUM[primeIndex(7)] -= steps;
+                            // frac 9
+                            NUM[primeIndex(11)] -= 1;
+                            NUM[primeIndex(13)] += 1;
+                            fracIndex = 4;
+                            cnt += steps + 1;
+                        } else {
+                            // this counts the steps for the repeated 0-1-0-1 cycles (plus on 0 execution)
+                            const steps: Integer = pow13;
+                            NUM[primeIndex(2)] += steps;
+                            NUM[primeIndex(3)] += steps;
+                            NUM[primeIndex(5)] -= steps;
+                            NUM[primeIndex(7)] -= steps;
+                            fracIndex = 0;
+                            cnt += steps;
+                        }
+                    } else {
+                        NUM[primeIndex(7)] -= 1;
+                        NUM[primeIndex(13)] -= 1;
+                        NUM[primeIndex(17)] += 1;
+                        if (NUM[primeIndex(3)] > 0 && NUM[primeIndex(17)] > 0) {
+                            fracIndex = 2;
+                        } else if (NUM[primeIndex(17)] > 0) {
+                            fracIndex = 8;
+                        } else {
+                            throw `case 0 :: ${NUM}`;
+                        }
+                        cnt += 1;
+                    }
+                    break;
+                case 2: {
+                    // frac 2
+                    NUM[primeIndex(19)] += 1;
+                    NUM[primeIndex(13)] -= 1;
+                    NUM[primeIndex(17)] -= 1;
+                    // cycle of frac 3-6-3-6
+                    const steps: Integer = NUM[primeIndex(2)];
+                    NUM[primeIndex(5)] += steps;
+                    NUM[primeIndex(2)] -= steps;
+                    // frac 7
+                    NUM[primeIndex(7)] += 1;
+                    NUM[primeIndex(11)] += 1;
+                    NUM[primeIndex(19)] -= 1;
+                    if (NUM[primeIndex(3)] > 0 && NUM[primeIndex(11)] > 0) {
+                        fracIndex = 4;
+                    } else if (NUM[primeIndex(11)] > 0) {
+                        NUM[primeIndex(11)] -= 1;
+                        NUM[primeIndex(13)] += 1;
+                        fracIndex = 0;
+                        cnt += 1;
+                    }
+                    cnt += steps + 1;
+                }
+                    break;
+                case 4:
+                    // this counts the steps for the repeated 4-5-4-5 cycles
+                    const steps: Integer = NUM[primeIndex(3)];
+                    NUM[primeIndex(3)] = 0;
+                    NUM[primeIndex(7)] += steps;
+                    cnt += 2 * steps; // 2 steps for each cycle
+                    // goes to frac 10;
+                    NUM[primeIndex(11)] -= 1;
+                    NUM[primeIndex(13)] += 1;
+                    cnt += 1;
+                    fracIndex = 0;
+                    break;
+                case 8:
+                    NUM[primeIndex(17)] -= 1;
+                    fracIndex = 11;
+                    cnt++;
+                    if (NUM.slice(1).every(v => v === 0)) {
+                        console.log(`cnt=${cnt} power2=${NUM[0]}`);
+                    }
+                    break;
+                case 11:
+                    // frac 11
+                    const steps11: Integer = NUM[2];
+                    NUM[3] = steps11;
+                    NUM[5] = steps11;
+                    cnt += steps11;
+                    // frac 12 (conditional)
+                    const steps12: Integer = NUM[7];
+                    if (steps12 > 0) {
+                        NUM[7] = 0;
+                        cnt += steps12;
+                    }
+                    // frac 13
+                    NUM[5] += 1;
+                    NUM[11] += 1;
+                    cnt += 1;
+                    fracIndex = 4;
+                    break;
+                default:
+                    throw `error ${cnt} ${NUM}`;
+            }
+        }
+    }
+
+
     const f2PN: PowerNumber = new PowerNumber().mul(3, -1).mul(17, -1).mul(2, -1).mul(23, 1).mul(7, 1).mul(11, 1).mul(19, -1);
 
     export function wheel(pownum: PowerNumber, frac: Integer): void {
@@ -75,18 +207,18 @@ for n in 2^n
             switch (frac) {
                 case 0:
                     pownum.mulBases(facNumers[frac]).divBases(facDenoms[frac]);
-                    frac = [1,2,8].find( f => pownum.divisibleBases(facDenoms[f]) ) as Integer;
+                    frac = [1, 2, 8].find(f => pownum.divisibleBases(facDenoms[f])) as Integer;
                     break;
                 case 1:
                     pownum.mulBases(facNumers[frac]).divBases(facDenoms[frac]);
-                    frac = [0,9].find( f => pownum.divisibleBases(facDenoms[f]) ) as Integer;
+                    frac = [0, 9].find(f => pownum.divisibleBases(facDenoms[f])) as Integer;
                     break;
                 case 2:
                     const diffPow: Integer = pownum.getPower(2) - 1;
                     f2PN.mul(2, -diffPow).mul(5, diffPow);
                     pownum.multiply(f2PN);
                     cnt += 3 + 2 * diffPow;
-                    frac = [4,10].find( f => pownum.divisibleBases(facDenoms[f]) ) as Integer;
+                    frac = [4, 10].find(f => pownum.divisibleBases(facDenoms[f])) as Integer;
                     break;
                 // case 3:
                 //     frac = 6;
@@ -97,7 +229,7 @@ for n in 2^n
                     break;
                 case 5:
                     pownum.mulBases(facNumers[frac]).divBases(facDenoms[frac]);
-                    frac = [4,10].find( f => pownum.divisibleBases(facDenoms[f]) ) as Integer;
+                    frac = [4, 10].find(f => pownum.divisibleBases(facDenoms[f])) as Integer;
                     break;
                 // case 6:
                 //     frac = [3,7].find( f => pownum.divisibleBases(facDenoms[f]) ) as Integer;
@@ -135,11 +267,11 @@ for n in 2^n
                     break;
                 case 11:
                     pownum.mulBases(facNumers[frac]).divBases(facDenoms[frac]);
-                    frac = [11,12,13].find( f => pownum.divisibleBases(facDenoms[f]) ) as Integer;
+                    frac = [11, 12, 13].find(f => pownum.divisibleBases(facDenoms[f])) as Integer;
                     break;
                 case 12:
                     pownum.mulBases(facNumers[frac]).divBases(facDenoms[frac]);
-                    frac = [12,13].find( f => pownum.divisibleBases(facDenoms[f]) ) as Integer;
+                    frac = [12, 13].find(f => pownum.divisibleBases(facDenoms[f])) as Integer;
                     break;
                 case 13:
                     pownum.mulBases(facNumers[frac]).divBases(facDenoms[frac]);
@@ -154,7 +286,9 @@ for n in 2^n
         for (let i = 0; i < numers.length; i++) {
             console.log(`(${facNumers[i]})/(${facDenoms[i]})`);
         }
-        wheel(new PowerNumber(2,1), 11);
+        // wheel(new PowerNumber(2, 1), 11);
+        NUM[0] = 1;
+        stepCount(11);
     }
 
 }
