b8618d9693d1b0fef7fed4a9c190d2fa65e3d637
rmanchu <riyaz.mansoor@gmail.com>
2023-11-06 23:42:50 +0500

    .

--- 158.js
+++ 158.js
@@ -1,25 +1,22 @@
-
-import { Integer } from "./common";
-
-namespace E158 {
-
-    const ALPHABETS: Integer = 26;
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var E158;
+(function (E158) {
+    const ALPHABETS = 26;
     // canche of Map<LENGTH, Map<START, Count>>
-    const CACHE: Map<Integer, Map<Integer, Integer>> = new Map();
-
-    function countAllFixedLength(pos: Integer, trekked:Integer[], LENGTH: Integer): Integer {
+    const CACHE = new Map();
+    function countAllFixedLength(pos, trekked, LENGTH) {
         if (pos == LENGTH) {
             // console.log(trekked);
             return 1;
         }
-        const startVal: Integer = 1 + (trekked[trekked.length - 1] ?? 0);
-        const remLen: Integer = LENGTH - pos;
+        const startVal = 1 + (trekked[trekked.length - 1] ?? 0);
+        const remLen = LENGTH - pos;
         if (!CACHE.has(remLen)) {
             CACHE.set(remLen, new Map());
         }
-        const cache: Map<Integer, Integer> = CACHE.get(remLen) as Map<Integer, Integer>;
-        let cnt: Integer = cache.get(startVal) ?? -1;
+        const cache = CACHE.get(remLen);
+        let cnt = cache.get(startVal) ?? -1;
         if (cnt < 0) {
             cnt = 0;
             for (let num = startVal; num <= ALPHABETS; num++) {
@@ -30,47 +27,50 @@ namespace E158 {
         }
         return cnt;
     }
-
-    function trek(pos: Integer, trekked: Integer[], length: Integer, trekOptions: Integer[], dipped: boolean): Integer {
+    function trek(pos, trekked, length, trekOptions, dipped) {
         if (pos == length) {
-            let dips: Integer = 0;
-            trekked.forEach( (v, i) => {
-                if (v < trekked[i-1] ?? -1 ) dips++;
-            } );
+            let dips = 0;
+            trekked.forEach((v, i) => {
+                if (v < trekked[i - 1] ?? -1)
+                    dips++;
+            });
             if (dips > 1) {
                 console.log(trekked);
                 throw `multi dips`;
                 // return 0;
-            } else if (dips == 1) {
+            }
+            else if (dips == 1) {
                 // console.log(trekked);
                 return 1;
-            } else {
+            }
+            else {
                 // console.log("dips 0 :: ", trekked);
                 return 0;
             }
         }
-        const lastVal: Integer = trekked[trekked.length - 1] ?? -1;
-        let cnt: Integer = 0;
+        const lastVal = trekked[trekked.length - 1] ?? -1;
+        let cnt = 0;
         for (const num of trekOptions) {
-            let newOptions: Integer[] = trekOptions.filter( v  => v  != num );
+            let newOptions = trekOptions.filter(v => v != num);
             if (num < lastVal) { // || dipped) {
+                if (dipped)
+                    continue;
                 dipped = true;
-                newOptions = newOptions.filter( v => v > num);
-                if (newOptions.length + 1 < length - pos) break;
+                newOptions = newOptions.filter(v => v > num);
+                if (newOptions.length + 1 < length - pos)
+                    break;
             }
             cnt += trek(pos + 1, trekked.concat([num]), length, newOptions, dipped);
         }
         return cnt;
     }
-
-    export function run(): void {
-        const options: Integer[] = Array(26).fill(1).map( (v, i) => v+i );
+    function run() {
+        const options = Array(26).fill(1).map((v, i) => v + i);
         for (let i = 3; i <= ALPHABETS; i++) {
             console.log(i, trek(0, [], i, options, false));
         }
         // console.log(CACHE);
     }
-
-}
-
+    E158.run = run;
+})(E158 || (E158 = {}));
 E158.run();
